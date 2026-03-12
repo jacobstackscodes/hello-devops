614e96e975f8bef5416ff267fbd9f6d6458f4fcd
jacobstackscodes <jacobdaniel220@gmail.com>
2026-03-12 09:53:08 +0000

    Made some more changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 print("-" * 40)
 print("Hello Mindgate! DevOps Pipeline Success.")
-print("The App is running inside a Docker Container.")
+print("The App is running inside a Docker Container, and its a sucess.")
 print("-" * 40)
